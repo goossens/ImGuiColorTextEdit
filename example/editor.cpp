529ec44270522f00dc0dce6c65eaeb5c1d4eac7f
Johan Goossens <johan@cox.net>
2026-08-01 07:52:54 -0400

    Fixed mingw compiler warning in example (#70)

--- example/editor.cpp
+++ example/editor.cpp
@@ -16,9 +16,15 @@
 #include <fstream>
 
 #ifdef _WIN32
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifdef APIENTRY
 #undef APIENTRY
+#endif
 #include <shlobj.h>
 #include <windows.h>
 #else
